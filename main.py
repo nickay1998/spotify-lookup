7ffdfeb24c771c7325fbfbc1f38f599bc1514d2d
Nicolas Au-Yeung <nickay1998@gmail.com>
2025-01-04 09:41:48 +0800

    chore: add streamlit page config

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 from spotify import authenticate_spotify
 from streamlit import session_state as ss
 
+st.set_page_config(page_title="Spotify Analysis", layout="wide", initial_sidebar_state="collapsed")
+
 authenticate_spotify()
 
 st.write(f'Connect to spotify API! Token: \'{ss["token"]}\'')
